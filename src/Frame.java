9a4deaf01a936f8914bf69ff39c4f91cb2d7462a
zombi3123 <joshuacarpenterjones@gmail.com>
2019-05-13 14:45:27 +0100

    Update Frame.java

--- src/Frame.java
+++ src/Frame.java
@@ -7,29 +7,49 @@ import java.util.Random;
 public class Frame extends JPanel implements MouseListener, ActionListener, MouseMotionListener {
     private ArrayList<Cell> columns;
     private ArrayList<ArrayList<Cell>> rows;
-    private int cellLength;
-    private Color dead;
+    private int cellLength,JFrameWidth,JFrameHeight;
+    private Color dead,killDefaultColor;
     private Timer tm;
-    private JButton Start,stop,randomize;
+    private int border;
+    private JButton Start,stop,randomize,kill,step,reset,drawLine;
     private Random randNum;
-    public Frame(){
-        tm=new Timer(10,this);
+    private boolean killCells,drawLineMode;
+    public Frame(int JFrameWidth,int JFrameHeight){
+        tm=new Timer(1,this);
         rows=new ArrayList<>();
         columns= new ArrayList<>();
-        this.setSize(new Dimension(1750, 1000));
-        this.cellLength=10;
+        this.cellLength=3;
         this.dead=Color.BLACK;
-
+        this.border=1;
+        this.JFrameWidth=JFrameWidth;
+        this.JFrameHeight=JFrameHeight;
+        this.killCells=false;
+        this.drawLineMode=false;
+        this.setSize(new Dimension(this.JFrameWidth, this.JFrameHeight));
         randNum=new Random();
         Start=new JButton("Start");
         stop=new JButton("Stop");
         randomize=new JButton("Randomize");
-        add(Start);
-        add(stop);
-        add(randomize);
-        Start.setBounds(0,0,50,25);
-        stop.setBounds(0,50,50,25);
-        randomize.setBounds(50,0,50,25);
+        kill=new JButton("Kill Cells");
+        step=new JButton("Step");
+        reset=new JButton("Reset");
+        drawLine =new JButton("Line");
+        ArrayList<JButton> JB = new ArrayList<>();
+        JB.add(Start);
+        JB.add(stop);
+        JB.add(randomize);
+        JB.add(kill);
+        JB.add(step);
+        JB.add(reset);
+        JB.add(drawLine);
+        int i=50;
+        for (JButton j:JB){
+            j.setBounds(i*2,0,50,20);
+            this.add(j);
+            i+=10;
+        }
+        killDefaultColor=kill.getBackground();
+        Color killActiveColour=Color.green;
         Start.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -49,11 +69,11 @@ public class Frame extends JPanel implements MouseListener, ActionListener, Mous
         randomize.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                for (int i = 0; i < rows.size(); i++) {
-                    for (int j = 0; j < rows.get(0).size(); j++){
+                for (int i = 1; i < rows.size()-1; i++) {
+                    for (int j = 1; j < rows.get(0).size()-1; j++){
                         Cell c=rows.get(i).get(j);
                         float a=randNum.nextFloat();
-                        if (a>0.9) {
+                        if (a>0.90) {
                             c.setAlive();
                         }
                     }
@@ -61,50 +81,105 @@ public class Frame extends JPanel implements MouseListener, ActionListener, Mous
                 repaint();
             }
         });
+        kill.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+              killCells=!killCells;
+              if (!killCells) {
+                    kill.setBackground(killDefaultColor);
+                }
+              else kill.setBackground(killActiveColour);
+            }
+        });
+        step.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ArrayList<Cell> deadCells=new ArrayList<Cell>();
+                ArrayList<Cell> bornCells=new ArrayList<Cell>();
+                for (int i = 1; i < rows.size()-1; i++) {
+                    for (int j = 1; j < rows.get(0).size()-1; j++){
+                        Cell c=rows.get(i).get(j);
+                        Cell cE=rows.get(i).get(j+1);
+                        Cell cW=rows.get(i).get(j-1);
+                        Cell cN=rows.get(i-1).get(j);
+                        Cell cS=rows.get(i+1).get(j);
+                        Cell cNE=rows.get(i-1).get(j+1);
+                        Cell cNW=rows.get(i-1).get(j-1);
+                        Cell cSE=rows.get(i+1).get(j+1);
+                        Cell cSW=rows.get(i+1).get(j-1);
+                        if(c.isAlive()){
+                            if(c.totalNeighbours(cE,cW,cN,cS,cNE,cNW,cSE,cSW)>=4){deadCells.add(c);}
+                            if(c.totalNeighbours(cE,cW,cN,cS,cNE,cNW,cSE,cSW)<2){deadCells.add(c);}
+                        }
+                        else{
+                            if(c.totalNeighbours(cE,cW,cN,cS,cNE,cNW,cSE,cSW)==3){bornCells.add(c);}
+                        }
+                        repaint();
+                    }
+                }
+                for(Cell c:deadCells){c.setDead();}
+                for(Cell c:bornCells){c.setAlive();}
+            }
+        });
+        reset.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                for (int i = 0; i < rows.size()-1; i++) {
+                    for (int j = 0; j < rows.get(0).size()-1; j++){
+                        Cell c=rows.get(i).get(j);
+                        c.setDead();
+                    }
+                }
+                repaint();
+            }
+        });
+        drawLine.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                drawLineMode = !drawLineMode;
+                if (drawLineMode) {
+                    killCells=false;
+                    drawLine.setBackground(killActiveColour);
+                }
+                else{
+                    drawLine.setBackground(killDefaultColor);
+                }
+            }
+        });
         repaint();
         createMap();
-        //startLife()
         addMouseListener(this);
         addMouseMotionListener(this);
         setFocusable(true);
-
-        //initComponents();
     }
 
     private void initComponents() {
     }
 
     public void createMap(){
+        //Draw grid. Initialize all cells to dead
             for(int columsi=0;columsi<getHeight()/cellLength;columsi++){
                 ArrayList<Cell> columns = new ArrayList<>();
                 for(int rowsi=0;rowsi<getWidth()/cellLength;rowsi++){
 
-                    Cell c=new Cell(rowsi*(cellLength+1),columsi*(cellLength+1),cellLength,false);
+                    Cell c=new Cell(rowsi*(cellLength+border),columsi*(cellLength+border),cellLength,false);
                     columns.add(c);
                     repaint();
                 }
                 rows.add(columns);
         }
-        System.out.println(columns.size());
 
-
-        System.out.println(rows.get(3).size());
+        System.out.println(rows.size()*rows.get(0).size());
     }
-    public void startLife(){
-
-
-
-    }
-
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         for (int i = 0; i < rows.size(); i++) {
             for (int j = 0; j < rows.get(0).size(); j++){
                 Cell c=rows.get(i).get(j);
                 if (c.isAlive()) {
-                    g.setColor(Color.yellow);
+                    g.setColor(Color.black);
                 } else {
-                    g.setColor(Color.magenta);
+                    g.setColor(Color.white);
                 }
             c.draw(g);
         }
@@ -112,41 +187,67 @@ public class Frame extends JPanel implements MouseListener, ActionListener, Mous
     }
 
     @Override
-    public void mouseClicked(MouseEvent e) {
-
-    }
+    public void mouseClicked(MouseEvent e) {}
 
     @Override
     public void mousePressed(MouseEvent e) {
+        if (killCells){
         for (int i = 1; i < rows.size()-1; i++) {
             for (int j = 1; j < rows.get(0).size()-1; j++){
                 Cell c=rows.get(i).get(j);
-                if(e.getX()>=c.getTlx()&&e.getX()<c.getTlx()+c.getCellLength()&&e.getY()>= c.getTly()&&e.getY()<c.getTly()+c.getCellLength()){
-                    c.setAlive();
+                if(e.getX()>=c.getTlx()&&e.getX()<c.getTlx()+c.getCellLength()&&e.getY()>= c.getTly()&&e.getY()<c.getTly()+c.getCellLength()) {
+                    c.setDead();
                     repaint();
+                }
                 }
             }
         }
+        else if(!killCells && !drawLineMode){
+            outerLoop:
+            for (int i = 1; i < rows.size()-1; i++) {
+                for (int j = 1; j < rows.get(0).size()-1; j++){
+                    Cell c=rows.get(i).get(j);
+                    if(e.getX()>=c.getTlx()&&e.getX()<c.getTlx()+c.getCellLength()&&e.getY()>= c.getTly()&&e.getY()<c.getTly()+c.getCellLength()) {
+                        c.setAlive();
+                        repaint();
+                        break outerLoop;
+                    }
+                }
+            }
+        }
+        else if(drawLineMode=true){
+            outerLoop:
+            for (int i = 0; i < rows.size()-1; i++) {
+                for (int j = 0; j < rows.get(0).size()-1; j++){
+                    Cell c=rows.get(i).get(j);
+                    if(e.getX()>=c.getTlx()&&e.getX()<c.getTlx()+c.getCellLength()&&e.getY()>= c.getTly()&&e.getY()<c.getTly()+c.getCellLength()) {
+                        for (int k = rows.indexOf(c); k < rows.get(0).size()-1; k++){
+                            if(k+j>rows.get(0).size()-1){break outerLoop;}
+                            else{rows.get(i).get(j+k).setAlive();}
+                            repaint();
 
-    }
+                        }
+                        //break outerLoop;
+                    }
+                }
+            }
+
+        }
+        }
+
+
 
     @Override
-    public void mouseReleased(MouseEvent e) {
-
-    }
+    public void mouseReleased(MouseEvent e) {}
 
     @Override
-    public void mouseEntered(MouseEvent e) {
-
-    }
+    public void mouseEntered(MouseEvent e) {}
 
     @Override
-    public void mouseExited(MouseEvent e) {
-
-    }
-
+    public void mouseExited(MouseEvent e) {}
 
     @Override
+    //When the timer ticks, action is performed
     public void actionPerformed(ActionEvent e) {
         ArrayList<Cell> deadCells=new ArrayList<Cell>();
         ArrayList<Cell> bornCells=new ArrayList<Cell>();
@@ -162,39 +263,50 @@ public class Frame extends JPanel implements MouseListener, ActionListener, Mous
                 Cell cSE=rows.get(i+1).get(j+1);
                 Cell cSW=rows.get(i+1).get(j-1);
                 if(c.isAlive()){
-                if(c.totalNeighbours(cE,cW,cN,cS,cNE,cNW,cSE,cSW)>3){deadCells.add(c);}
+                if(c.totalNeighbours(cE,cW,cN,cS,cNE,cNW,cSE,cSW)>=4){deadCells.add(c);}
                 if(c.totalNeighbours(cE,cW,cN,cS,cNE,cNW,cSE,cSW)<2){deadCells.add(c);}
             }
                 else{
                     if(c.totalNeighbours(cE,cW,cN,cS,cNE,cNW,cSE,cSW)==3){bornCells.add(c);}
                 }
                 repaint();
-
             }
-
         }
         for(Cell c:deadCells){c.setDead();}
         for(Cell c:bornCells){c.setAlive();}
-
-
-
     }
 
     @Override
+    //When mouse dragged across screen, draw cells according to mouses position
     public void mouseDragged(MouseEvent e) {
+    if(killCells){
+        outerloop:
         for (int i = 1; i < rows.size()-1; i++) {
             for (int j = 1; j < rows.get(0).size()-1; j++){
                 Cell c=rows.get(i).get(j);
-                if(e.getX()>=c.getTlx()&&e.getX()<c.getTlx()+c.getCellLength()&&e.getY()>= c.getTly()&&e.getY()<c.getTly()+c.getCellLength()){
-                    c.setAlive();
+                if(c.isAlive()){
+                if(e.getX()>=c.getTlx()&&e.getX()<c.getTlx()+c.getCellLength()&&e.getY()>= c.getTly()&&e.getY()<c.getTly()+c.getCellLength()) {
+                    c.setDead();
                     repaint();
+                    break outerloop;
+                }
+                }
+            }
+        }
+    }
+          else{
+            for (int i = 1; i < rows.size()-1; i++) {
+                for (int j = 1; j < rows.get(0).size()-1; j++){
+                    Cell c=rows.get(i).get(j);
+                    if(e.getX()>=c.getTlx()&&e.getX()<c.getTlx()+c.getCellLength()&&e.getY()>= c.getTly()&&e.getY()<c.getTly()+c.getCellLength()) {
+                        c.setAlive();
+                        repaint();
+                    }
                 }
             }
         }
     }
 
     @Override
-    public void mouseMoved(MouseEvent e) {
-
-    }
+    public void mouseMoved(MouseEvent e) {}
 }
