6aa4d6fe6977af6fef78a3f7136b43ebf3107957
zombi3123 <joshuacarpenterjones@gmail.com>
2019-05-13 14:51:04 +0100

    Update Window.java

--- src/Window.java
+++ src/Window.java
@@ -1,14 +1,16 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class Window extends JFrame {
+
     public Window(int x, int y) {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(x, y);
-        this.setVisible(true);
-        this.setLayout(null);
         this.setResizable(true);
+        this.setLayout(null);
+        getContentPane().setBackground(Color.BLACK);
     }
 
 }
